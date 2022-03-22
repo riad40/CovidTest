e4a96afe545900f9ca3a6c8214d9f1b23fd58865
riad40 <R.Abdelaziz@student.youcode.ma>
2022-03-22 16:53:53 +0100

    make (previous & next) buttons work

---
+++ assets/js/quizz.js
@@ -0,0 +1,54 @@
+// previous and next buttons
+
+const previous = document.querySelectorAll('#previous')
+const next = document.querySelectorAll('#next')
+
+// Questions 
+
+document.querySelector('#Question1')
+document.querySelector('#Question2')
+document.querySelector('#Question3')
+document.querySelector('#Question4')
+document.querySelector('#Question5')
+document.querySelector('#Question6')
+document.querySelector('#Question7')
+document.querySelector('#Question8')
+document.querySelector('#Question9')
+document.querySelector('#Question10')
+document.querySelector('#Question11')
+document.querySelector('#Question12')
+document.querySelector('#Question13')
+document.querySelector('#Question14')
+document.querySelector('#Question15')
+
+// 
+
+let i = 1
+
+/* ====== next button ====== */
+
+next.forEach((btn) => {
+    btn.addEventListener('click', nextF)
+})
+
+function nextF() {
+    document.querySelector('#Question' + i).classList.remove('active')
+    document.querySelector('#Question' + i).classList.add('inactive')
+    i++
+    document.querySelector('#Question' + i).classList.remove('inactive')
+    document.querySelector('#Question' + i).classList.add('active')
+}
+
+/* ====== previous button ====== */
+
+previous.forEach((btn) => {
+    btn.addEventListener('click', previousF)
+})
+
+function previousF() {
+    document.querySelector('#Question' + i).classList.remove('active')
+    document.querySelector('#Question' + i).classList.add('inactive')
+    i--
+    document.querySelector('#Question' + i).classList.remove('inactive')
+    document.querySelector('#Question' + i).classList.add('active')
+}
