be333045d69b7cf0d0bfe2be79e7fb798fcb6820
riad40 <R.Abdelaziz@student.youcode.ma>
2022-03-29 14:22:23 +0000

    updated questions

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -37,60 +37,61 @@ const questions = [
     {
         title: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Quelle est votre température corporelle ?',
         type: 'number',
         numberType: 'temp',
-        spanName: 'ans',
-        min: 15,
-        max: 90,
+        spanName: 'degré',
+        min: 34,
+        max: 42,
         answer: ''
     },
     {
         title: 'Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
+        title: 'Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?',
+        type: 'radio',
+        options: ['Oui', 'Non'],
+        answer: ''
+    },    
+    {
         title: 'Ces derniers jours, avez-vous un mal de gorge ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
         type: 'radio',
-        options: ['Oui', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
-        title: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
-        type: 'radio',
-        options: ['Oui', 'NON'],
-        answer: ''
-    },{
         title: 'Actuellement, comment vous vous sentez ?',
         type: 'radio',
         options: ['Bien', 'Assez bien', 'Fatigue', 'Trés Fatigué(e)'],
@@ -109,81 +110,78 @@ const questions = [
         title: 'Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
         type: 'number',
         numberType: 'poid',
-        spanName: 'ans',
-        min: 15,
-        max: 110,
+        spanName: 'kg',
+        min: 20,
+        max: 250,
         answer: ''
-    },{
+    },
+    {
         title: 'Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
         type: 'number',
-        numberType: 'age',
-        spanName: 'ans',
-        min: 15,
-        max: 110,
+        numberType: 'taille',
+        spanName: 'cm',
+        min: 80,
+        max: 250,
         answer: ''
     },
     {
         title: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
-        answer: ''
-    },
-    {
-        title: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
-        type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Êtes-vous diabétique ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Avez-vous ou avez-vous eu un cancer ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Avez-vous une insuffisance rénale chronique dialysée ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Avez-vous une maladie chronique du foie ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Êtes-vous enceinte ?',
         type: 'radio',
-        options: ['OUI', 'NON', 'HOMME'],
+        options: ['Oui', 'Non', 'HOMME'],
         answer: ''
     },
     {
         title: 'Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
     },
     {
         title: 'Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).',
         type: 'radio',
-        options: ['OUI', 'NON'],
+        options: ['Oui', 'Non'],
         answer: ''
-    },
+    }
 
 ]
+
+console.log(questions.length)
 
 // display questions
 
@@ -210,7 +208,7 @@ function nextQuestion() {
             previous.classList.remove('inactive')
             switchBtns.classList.add('btns')
             switchBtns.classList.remove('btnsQ1')
-        } if (compteur == 3) {
+        } if (compteur == 22) {
             next.textContent = 'finish'
         }
         questionTitle.innerText = questions[compteur].title
