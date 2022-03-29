7f1a0c2ecf4837f92a322a29a8ad86f54bfb1f2c
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-03-29 13:51:04 +0000

    ADDED question

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -35,22 +35,22 @@ function quizStart() {
 
 const questions = [ 
     {
-        title: 'wach nta mrid ?',
+        title: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
         type: 'radio',
-        options: ['yaap', 'noop'],
+        options: ['Oui', 'NON'],
         answer: ''
     },
     {
-        title: 'l3mer ?',
+        title: 'Quelle est votre température corporelle ?',
         type: 'number',
-        numberType: 'age',
+        numberType: 'temp',
         spanName: 'ans',
         min: 15,
         max: 90,
         answer: ''
     },
     {
-        title: 'lharara ?',
+        title: 'Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?',
         type: 'number',
         numberType: 'lharara',
         spanName: 'degré',
@@ -59,14 +59,132 @@ const questions = [
         answer: ''
     },
     {
-        title: 'lkilwat ?',
-        type: 'number',
-        numberType: 'lkilwat',
-        spanName: 'kg',
-        min: 30,
-        max: 120,
+        title: 'Ces derniers jours, avez-vous un mal de gorge ?',
+        type: 'radio',
+        options: ['Oui', 'NON'],
         answer: ''
-    }
+    },
+    {
+        title: 'Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.',
+        type: 'radio',
+        options: ['Oui', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?',
+        type: 'radio',
+        options: ['Oui', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?',
+        type: 'radio',
+        options: ['Oui', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
+        type: 'radio',
+        options: ['Oui', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
+        type: 'radio',
+        options: ['Oui', 'NON'],
+        answer: ''
+    },{
+        title: 'Actuellement, comment vous vous sentez ?',
+        type: 'radio',
+        options: ['Bien', 'Assez bien', 'Fatigue', 'Trés Fatigué(e)'],
+        answer: ''
+    },
+    {
+        title: 'Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.',
+        type: 'number',
+        numberType: 'age',
+        spanName: 'ans',
+        min: 15,
+        max: 110,
+        answer: ''
+    },
+    {
+        title: 'Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+        type: 'number',
+        numberType: 'poid',
+        spanName: 'ans',
+        min: 15,
+        max: 110,
+        answer: ''
+    },{
+        title: 'Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+        type: 'number',
+        numberType: 'age',
+        spanName: 'ans',
+        min: 15,
+        max: 110,
+        answer: ''
+    },
+    {
+        title: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Êtes-vous diabétique ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous ou avez-vous eu un cancer ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous une insuffisance rénale chronique dialysée ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous une maladie chronique du foie ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Êtes-vous enceinte ?',
+        type: 'radio',
+        options: ['OUI', 'NON', 'HOMME'],
+        answer: ''
+    },
+    {
+        title: 'Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
+    {
+        title: 'Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).',
+        type: 'radio',
+        options: ['OUI', 'NON'],
+        answer: ''
+    },
 
 ]
 
